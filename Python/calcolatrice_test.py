e2161f65a3c27090deaacbda29c779b3287fed05
alessiobarraco <101570067+alessiobarraco@users.noreply.github.com>
2025-04-22 22:55:01 +0200

    fix (il test deve fallire)

--- Python/calcolatrice_test.py
+++ Python/calcolatrice_test.py
@@ -1,9 +1,10 @@
 import unittest
 from calcolatrice import somma
 
+
 class TestCalcolatrice(unittest.TestCase):
     def test_somma(self):
-        self.assertEqual(somma(4, 2), 6)
+        self.assertEqual(somma(4, 2), 7)
 
 if __name__ == '__main__':
     unittest.main()
